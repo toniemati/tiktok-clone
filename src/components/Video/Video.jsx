23feb0a3a1226e46e6c92307cef747a5e3d4e790
toniemati <matikas2000@gmail.com>
2021-12-18 09:45:39 +0100

    setup firebase, creating video component

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './Video.css';
+
+const Video = () => {
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (videoRef.current.paused)
+      videoRef.current.play();
+    else
+      videoRef.current.pause();
+  }
+
+  return (
+    <div className="video">
+      <video
+        ref={videoRef}
+        className="video__player"
+        loop
+        onClick={onVideoPress}
+        src="https://v16-webapp.tiktok.com/bef50c2ee4be1f5ce4df0f2e241a657e/61bdf00c/video/tos/useast2a/tos-useast2a-pve-0068/09226556a50b414898ddb0b772ca2000/?a=1988&br=2168&bt=1084&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FO6kag3-I&l=20211218082810010223085019158B345F&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajQzd2Y6Zmx1OTMzNzczM0ApZzozOzozNTw7Nzo5NDlpNWdrLmg0cjRnX2NgLS1kMTZzcy4uNjFhLmJeMl4xYDItNC86Yw%3D%3D&vl=&vr="
+      >
+      </video>
+      {/* <VideoFooter /> */}
+      {/* <VideoSideBar /> */}
+    </div>
+  )
+}
+
+export default Video
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkzA2h8_vORnGYt13jyR7I-VesU-1TIH8",
+  authDomain: "tiktok-clone-88d5b.firebaseapp.com",
+  projectId: "tiktok-clone-88d5b",
+  storageBucket: "tiktok-clone-88d5b.appspot.com",
+  messagingSenderId: "473298180606",
+  appId: "1:473298180606:web:4d1d72ffc09deea529c8af",
+  measurementId: "G-B8GD1L4LGE"
+};
